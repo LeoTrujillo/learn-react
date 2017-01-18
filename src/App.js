9573b05c29ff659df696eaf47fee3a0d79d3ef0d
Leo <leonardotrujillor@gmail.com>
2017-01-18 12:52:38 -0600

    Add more class and test 'this' in a class es6

--- src/App.js
+++ src/App.js
@@ -8,21 +8,76 @@ import logo from './logo.svg'
 // }
 
 
-function Clock(props){
-  return (
-    <div>
-      <h2>Hello world!</h2>
-      <h2>It is {props.date.toLocaleTimeString()}</h2>
-    </div>
-  );
+//This code is for make a button
+
+class Toggle extends React.Component{
+  constructor (props){
+    super(props);
+    this.state = {isToggleOn :true};
+
+    //This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+    // console.log('this is', this);
+  }
+
+  render(){
+    return(
+      // <button onClick={(e)=>this.handleClick(e)}>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON':'OFF'}
+      </button>
+    )
+  }
+}
+
+
+//Code for make a clock
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    this.setState({
+      date:new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>Hello world!</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    );
+  }
 }
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Clock date={new Date()}/>
-        <img className="App-logo" src={logo} alt="Leo"/>
+        <Clock/>
+        <img className="App-logo" src={logo} alt="Leo"/><br/>
+        <Toggle/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import App from './App';
 import './index.css';
 
 
-function ticky(){
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
-}
-setInterval(ticky, 1000);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
